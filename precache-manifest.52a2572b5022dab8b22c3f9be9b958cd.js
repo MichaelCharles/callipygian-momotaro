fe372f6603f9cb50d0879d069f91da057237de79
Michael Charles Aubrey <michael@akita-itlabo.com>
2019-05-07 21:24:22 +0900

    Updates

---
+++ precache-manifest.52a2572b5022dab8b22c3f9be9b958cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e5ea43e593331c870ba",
+    "url": "/callipygian-momotaro/static/css/main.c750df6d.chunk.css"
+  },
+  {
+    "revision": "5e5ea43e593331c870ba",
+    "url": "/callipygian-momotaro/static/js/main.5bb4a768.chunk.js"
+  },
+  {
+    "revision": "ac978c6084dc5bc7944a",
+    "url": "/callipygian-momotaro/static/js/runtime~main.dae0ed63.js"
+  },
+  {
+    "revision": "f5470452aec0bd61a447",
+    "url": "/callipygian-momotaro/static/js/2.4cbfd1a3.chunk.js"
+  },
+  {
+    "revision": "b68a9ea82d52a35ceadb931db013f4dd",
+    "url": "/callipygian-momotaro/index.html"
+  }
+];
