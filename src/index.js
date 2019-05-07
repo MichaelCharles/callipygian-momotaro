e46fabfef04251bb61d619d17f0dffb69abcf0ae
Michael Charles Aubrey <michael@akita-itlabo.com>
2019-05-07 19:01:27 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,129 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+class App extends React.Component {
+  state = {
+    phrase: "",
+    phrases: ["elephant", "scray", "coffee", "tofu"],
+    limit: 1,
+    results: []
+  };
+
+  handleAdd() {
+    let { phrases, phrase } = this.state;
+    phrases.push(phrase);
+    this.setState({ phrases: phrases, phrase: "" });
+  }
+
+  onPhraseChange(newPhrase) {
+    this.setState({
+      phrase: newPhrase
+    });
+  }
+
+  removePhrase(e, index) {
+    let { limit, phrases } = this.state;
+    phrases.splice(index, 1);
+    this.setState({ phrases });
+    if (limit >= phrases.length) {
+      limit = phrases.length;
+    }
+    this.setState({ limit });
+  }
+
+  increment() {
+    let { limit, phrases } = this.state;
+    limit++;
+    if (limit >= phrases.length) {
+      limit = phrases.length;
+    }
+    this.setState({ limit });
+  }
+
+  decrement() {
+    let { limit } = this.state;
+    this.setState({ limit: limit-- <= 0 ? "0" : limit });
+  }
+
+  getRandom() {
+    let { limit } = this.state;
+    let results = [];
+    let phrases = [].concat(this.state.phrases);
+    for (let i = 0; i < limit; i++) {
+      results.push(phrases.splice(Math.floor(Math.random() * phrases.length),1));
+    }
+    this.setState({ results });
+  }
+
+  render() {
+    const { limit, phrases, phrase, results } = this.state;
+    return (
+      <div>
+        <h1>Callipygian Momotaro</h1>
+        <input
+          type="text"
+          value={phrase}
+          onChange={e => this.onPhraseChange(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            this.handleAdd();
+          }}
+        >
+          +
+        </button>
+        <ul>
+          {phrases.map((item, index) => (
+            <li key={index}>
+              {item}{" "}
+              <span
+                className="clickable-text"
+                onClick={e => {
+                  this.removePhrase(e, index);
+                }}
+              >
+                [x]
+              </span>
+            </li>
+          ))}
+        </ul>
+        <div>
+          Limit: {limit} &nbsp;
+          <button
+            onClick={() => {
+              this.increment();
+            }}
+          >
+            +
+          </button>
+          <button
+            onClick={() => {
+              this.decrement();
+            }}
+          >
+            -
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              this.getRandom();
+            }}
+          >
+            Get Random
+          </button>
+        </div>
+        <div>
+          {results.map((result, index) => (
+            <p key={index}>{result}</p>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
